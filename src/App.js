28cd71d28170c29cb1cdc0588216235299834a1d
Brandon Green <jailhousejordans@gmail.com>
2019-09-12 12:43:22 -0500

    separated into components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import React from "react";
 import "./App.css";
+import CardList from './Components/CardList'
 
 function App() {
 
-  const [data, setData] = useState([])
-
-  useEffect(() => {
-    axios
-      .get('https://api.nasa.gov/planetary/apod?api_key=JOKttsNDn4qO47RXwsgVP5QcPNAiX5Ub51HrBcgd&date=2012-03-14')
-      .then(res => {
-        console.log(res.data)
-        setData(res.data)
-      })
-      .catch(err => {
-        console.log('err', err)
-      })
-  }, [])
 
   return (
     <div className="App">
-
-      <h1>{data.title}</h1>
-      <h3>{data.date}</h3>
-      <img src={data.url} />
-      <h3>{data.explanation}</h3>
+      <CardList />
     </div>
   );
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div>
-
+            {/* Naming conventions between this page and cardlist matter!!! */}
+            <h1>{props.title}</h1>
+            <h2>{props.date}</h2>
+            <img src={props.img} alt='space' />
+            <p>{props.explanation}</p>
         </div>
     )
 }
